3716439c54665b576b23d2477a3d656b2cde0456
Giordano Lanna <giordanoal2003@gmail.com>
2025-05-23 01:20:11 -0300

    Add unit tests for DailyMenu component and configure Vitest

---
+++ Frontend/src/componentes/templates/menu/daily-menu.test.tsx
@@ -0,0 +1,66 @@
+import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import {it, expect, vi } from 'vitest'
+import '@testing-library/jest-dom'
+import { DailyMenu } from './daily-menu'
+
+
+type Dish = { id: number; name: string; description: string }
+var dishesMock: Dish[] = []
+
+const mockFetchMenu = vi.fn().mockResolvedValue({
+  success: true,
+  message: 'Cardápio carregado com sucesso',
+  dishes: dishesMock,
+})
+
+const mockUpdateMenu = vi.fn().mockResolvedValue({
+  success: true,
+  message: 'Cardápio atualizado',
+})
+
+const mockAddDishByName = vi.fn((name: string, description: string) => {
+  dishesMock.push({ id: Date.now(), name, description })
+})
+
+const mockGetDishes = vi.fn(() => dishesMock)
+
+vi.mock('../../../models/Menu', () => ({
+  MenuDish: vi.fn().mockImplementation(() => ({
+    fetchMenu: mockFetchMenu,
+    updateMenu: mockUpdateMenu,
+    getDishes: mockGetDishes,
+    addDishByName: mockAddDishByName,
+    getDay: vi.fn().mockReturnValue('2025-05-22'),
+    setDishes: vi.fn(),
+    setDay: vi.fn(),
+    setEnterpriseId: vi.fn(),
+  })),
+}))
+
+
+it('adiciona prato e oculta formulário após salvar', async () => {
+  render(<DailyMenu idEnterprise={31}/>)
+
+  fireEvent.click(screen.getByLabelText('Adicionar prato'))
+
+  // 2. Verifica se formulário apareceu
+  expect(screen.getByPlaceholderText('Nome do prato')).toBeInTheDocument()
+  expect(screen.getByPlaceholderText('Modo de preparo')).toBeInTheDocument()
+
+  // 3. Preenche os campos
+  fireEvent.change(screen.getByPlaceholderText('Nome do prato'), {
+    target: { value: 'Arroz' }
+  })
+  fireEvent.change(screen.getByPlaceholderText('Modo de preparo'), {
+    target: { value: 'Cozinhar com alho e óleo' }
+  })
+
+  // 4. Clica em salvar
+  fireEvent.click(screen.getByText('Salvar'))
+
+  // 5. Verifica se prato foi adicionado
+  await waitFor(() => {
+    expect(screen.getByText('Arroz')).toBeInTheDocument()
+    expect(screen.getByText('Cozinhar com alho e óleo')).toBeInTheDocument()
+  })
+})
