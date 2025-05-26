7cba5ef803f8d2efa1f57a06ed383d3473569187
Giordano Lanna <giordanoal2003@gmail.com>
2025-05-25 21:57:24 -0300

    fix: correct casing issue in file names to resolve deployment errors

--- Frontend/src/componentes/templates/create-enterprise/api.ts
+++ Frontend/src/componentes/templates/create-enterprise/api.ts
@@ -18,7 +18,7 @@ type RegisterResponse = {
   status?: number
 }
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.BASE_URL
 
 export const registerEnterprise = async (
   user: EnterpriseRegisterData
