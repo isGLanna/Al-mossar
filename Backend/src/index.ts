573dd675415d4d10de8e9afdb4eeb105d03f29bb
Giordano Lanna <giordanoal2003@gmail.com>
2025-05-26 11:43:35 -0300

    por algum motivo eu apaguei o index.ts e tenho que repor agora

---
+++ Backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+import routes from './routes'
+
+const app = express();
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/api', routes);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor rodando em http://0.0.0.0:${PORT}`)
+})
