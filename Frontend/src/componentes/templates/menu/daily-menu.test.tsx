599a23b4bfe974993f032722380c92dd33151f79
Giordano Lanna <giordanoal2003@gmail.com>
2025-05-29 01:39:24 -0300

    Update .gitignore and fix API URL; enhance daily menu tests and adjust employee panel styles

--- Frontend/src/componentes/templates/menu/daily-menu.test.tsx
+++ Frontend/src/componentes/templates/menu/daily-menu.test.tsx
@@ -67,3 +67,23 @@ it('adiciona prato e oculta formulário após salvar', async () => {
   expect(screen.queryByText('Modo de preparo')).not.toBeInTheDocument()
   expect(screen.queryByText('Salvar')).not.toBeInTheDocument()
 })
+
+it('adiciona prato e oculta formulário após salvar', async () => {
+  render(<DailyMenu idEnterprise={31}/>)
+
+  fireEvent.click(screen.getByLabelText('Adicionar prato'))
+
+  // 3. Preenche os campos
+  fireEvent.change(screen.getByPlaceholderText('Nome do prato'), {
+    target: { value: 'Arroz' }
+  })
+  fireEvent.change(screen.getByPlaceholderText('Modo de preparo'), {
+    target: { value: 'Cozinhar com alho e óleo' }
+  })
+
+  // 4. Clica em salvar
+  fireEvent.click(screen.getByText('Salvar'))
+  
+  // 5. Verifica se prato foi adicionado e a função de inserir prato foi removida
+  await screen.findByText('Arroz')
+})
