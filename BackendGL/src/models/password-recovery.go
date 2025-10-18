98fd2e3cf7751729ede43c73536dd4f29ce902a6
Giordano Lanna <seu-email-no-github@example.com>
2025-10-17 22:56:50 -0300

    Resolvidos conflitos com main

---
+++ BackendGL/src/models/password-recovery.go
@@ -0,0 +1,11 @@
+package models
+
+import (
+	"time"
+)
+
+type PasswordRecoveryCode struct {
+	Email     string    `gorm:"primaryKey;size:100;not null" json:"email"`
+	Code      string    `gorm:"size:5;not null" json:"code"`
+	ExpiresAt time.Time `gorm:"not null" json:"expires_at"`
+}
