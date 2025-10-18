20eef64f08db600cbd9ebdd7be365073a1beebcc
Giordano Almeida Lanna <167474669+isGLanna@users.noreply.github.com>
2025-10-17 22:35:19 -0300

    Add files via upload

--- BackendGL/src/routes/enterprise-routes.go
+++ BackendGL/src/routes/enterprise-routes.go
@@ -9,9 +9,8 @@ import (
 func RegisterEnterpriseRoutes(api *gin.RouterGroup) {
 	enterprise := api.Group("/enterprise")
 	{
-		enterprise.POST("/:id/theme", controllers.CreateEnterpriseTheme)
+		enterprise.POST("/:id/theme", controllers.CreateOrUpdateEnterpriseTheme)
 		enterprise.GET("/:id/theme", controllers.GetEnterpriseTheme)
-		enterprise.PUT("/:id/theme", controllers.UpdateTheme)
 	}
 
 }
