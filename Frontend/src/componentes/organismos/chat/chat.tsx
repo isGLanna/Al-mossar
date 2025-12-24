865669e2f39ece816f672994af1e1889ce3edec6
Giordano Lanna <seu-email-no-github@example.com>
2025-12-23 23:19:53 -0300

    fix (UI): correções e melhorias na responsividade do botão do chat, campo de mensagem adicionado e melhoria da visualização dos componentes

--- Frontend/src/componentes/organismos/chat/chat.tsx
+++ Frontend/src/componentes/organismos/chat/chat.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { IoIosPeople } from "react-icons/io"
 import { HiMiniMinus } from "react-icons/hi2";
 import { Avatar } from "../../atomos/avatar/avatar"
+import { AiOutlineSend } from "react-icons/ai";
 import "./chat.scss"
 
 interface ChatProps {
@@ -25,35 +26,47 @@ export function Chat({ isOpen, setIsOpen }: ChatProps) {
     { id: 2, text: "Oi! Eu queria saber o horário de funcionamento.", isUser: true, photo: null  },
     { id: 3, text: "Claro! Estamos abertos das 10h às 22h todos os dias.", isUser: false, photo: null  },
     { id: 4, text: "Perfeito, obrigado!", isUser: true, photo: null  },
+    { id: 1, text: "Olá! Seja bem-vindo ao suporte 😊", isUser: false, photo: null },
+    { id: 2, text: "Oi! Eu queria saber o horário de funcionamento.", isUser: true, photo: null  },
+    { id: 3, text: "Claro! Estamos abertos das 10h às 22h todos os dias.", isUser: false, photo: null  },
+    { id: 4, text: "Perfeito, obrigado!", isUser: true, photo: null  },
+    { id: 1, text: "Olá! Seja bem-vindo ao suporte 😊", isUser: false, photo: null },
+    { id: 2, text: "Oi! Eu queria saber o horário de funcionamento.", isUser: true, photo: null  },
+    { id: 3, text: "Claro! Estamos abertos das 10h às 22h todos os dias.", isUser: false, photo: null  },
+    { id: 4, text: "Perfeito, obrigado!", isUser: true, photo: null  },
+
   ]
 
   return (
-      <div className="chat-container">
-          <header>
-              <button> <IoIosPeople size={30} /> </button>
-              <button onClick={() => setIsOpen(false)}> <HiMiniMinus size={30} /> </button>
-          </header>
+    <div className="chat-container">
+      <header className="chat-header">
+        <button> <IoIosPeople size={30} /> </button>
+        <button onClick={() => setIsOpen(false)}> <HiMiniMinus size={30} /> </button>
+      </header>
 
-          <section className="message-area">
-            {messages.map((message) => (
-                <article className="message">
-                    <div className={`avatar-container ${message.isUser ? "my" : ""}`}>
-                      {!message.isUser && (<Avatar photo={message.photo}/>)}
-                        <div className="message-content">
-                          <span className="sender-name font-semibold">Giordano Lanna</span>
-                          <span className="timestamp">11:50</span>
-                          <p className="message-text">{message.text}</p>
-                        </div>
-                      {message.isUser && (<Avatar photo={message.photo}/>)}
-                    </div>
-                </article>
-            ))}
-          </section>
+      <section className="message-area">
+        {messages.map((message) => (
+          <article className="message avatar">
+            {!message.isUser && (<Avatar photo={message.photo} />)}
+            <div className="message-content">
+              <div className="message-header">
+                <span className="sender-name font-semibold">Giordano Lanna</span>
+                <span className="timestamp">11:50</span>
+              </div>
+              <p className="message-text">{message.text}</p>
+            </div>
 
-          <div className="input-chat">
-              <input type="text" name="" id="" className="text-input"/>
-              <button className="send-button"></button>
-          </div>
-      </div>
+            {message.isUser && (<Avatar photo={message.photo}/>)}
+          </article>
+        ))}
+      </section>
+
+      <section className="message-sender">
+        <form className="flex flex-row h-0 w-full">
+          <textarea name="message" id="message-field" className="message-input" spellCheck={false}></textarea>
+          <button className="sender"> <AiOutlineSend size={20} color="white"/> </button>
+        </form>
+      </section>
+    </div>
   )
 }
